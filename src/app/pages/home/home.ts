a8629ceab347f3f787983d5b7e52db4888fc8a19
Engelberth Sneyder Pico Blanco <engelberthyam@gmail.com>
2026-08-16 18:17:50 -0500

    Modifica todo la pagina de home y agrega call to action to calendly

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,12 +3,11 @@ import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 
 import { ScrollRevealDirective } from '../../shared/scroll-reveal.directive';
-import { ImagePlaceholder } from '../../shared/image-placeholder/image-placeholder';
 import { LangLinkPipe } from '../../shared/lang-link.pipe';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, TranslatePipe, ScrollRevealDirective, ImagePlaceholder, LangLinkPipe],
+  imports: [RouterLink, TranslatePipe, ScrollRevealDirective, LangLinkPipe],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
